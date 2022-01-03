2ed52b54eb660497172871a4d308285a716cce29
osushizuki <imaokanorihiko@icloud.com>
2022-01-03 22:21:39 +0900

    バリデーション

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -2,4 +2,9 @@ class Schedule < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :interview
 
+  validates :company, presence: true
+  validates :interview_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :dating, presence: true, format:{with: /\A[0-9]+\z/}
+  validates :start_time, presence: true
+  validates :end_time, presence: true
 end
